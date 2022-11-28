56f92e8dc5a44cd398b658cd273800207313d17e
Manjindergit <manjindergrewal1997@gmail.com>
2022-11-28 00:12:37 -0800

    done

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,18 +1,41 @@
 import React from "react";
 
-const Message=()=>{
-    return(
-        <div className="message user">
-            <div className="messageInfo">
-                <img src="https://makerkids.com/wp-content/uploads/2016/09/Mark-Zuckerberg.jpg" alt="Img" />
-                <span>just now</span>
-            </div>
-            <div className="messageContent">
-                <p>hola</p>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1yZlCcvALn1OXU3bdyMpXrTDcZ3cwWEA_OEaoQxhgaLeiD-w7g4wKgCsJhGL_twZuSH8&usqp=CAU" alt="Content" />
-            </div>
-            </div>
-    )
-}
+import { useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+
+const Message = ({ message }) => {
+    const { currentUser } = useContext(AuthContext);
+    const { data } = useContext(ChatContext);
+  
+    const ref = useRef();
+  
+    useEffect(() => {
+      ref.current?.scrollIntoView({ behavior: "smooth" });
+    }, [message]);
+  
+    return (
+      <div
+        ref={ref}
+        className={`message ${message.senderId === currentUser.uid && "owner"}`}
+      >
+        <div className="messageInfo">
+          <img
+            src={
+              message.senderId === currentUser.uid
+                ? currentUser.photoURL
+                : data.user.photoURL
+            }
+            alt=""
+          />
+          <span>just now</span>
+        </div>
+        <div className="messageContent">
+          <p>{message.text}</p>
+          {message.img && <img src={message.img} alt="" />}
+        </div>
+      </div>
+    );
+  };
 
 export default Message
